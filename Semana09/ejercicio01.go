4c56c5a6dc23cb116e19e2ba0562e10a338ee68d
RafaelAnderson <rp_9966@hotmail.com>
2021-12-01 23:29:59 -0500

    Semana 7

--- Semana09/ejercicio01.go
+++ Semana09/ejercicio01.go
@@ -1,53 +1,13 @@
-// Algoritmo 6.8 Producer-Consumer - Estructura Mutex
 package main
 
-import (
-	"fmt"
-	"sync"
-)
-
-var buffer []int
-var tope = 10 // buffer finito
-
-func productor(m *sync.Mutex) {
-
-	var d int
-	for {
-		m.Lock()
-		if len(buffer) < tope {
-			// Producir
-			d++
-			buffer = append(buffer, d) // Agregar el valor d al slide
-			fmt.Printf("Produciendo %d\n", d)
-		} else {
-			fmt.Println("Buffer lleno!!")
-		}
-		m.Unlock()
-	}
+func productor(id, ch chan string) {
 }
 
-func consumidor(m *sync.Mutex) {
-	var d int
-	for {
-		m.Lock()
-		if len(buffer) > 0 {
-			// Producir
-			d = buffer[0]
-			buffer = buffer[1:] // Agregar el valor d al slide
-			fmt.Printf("Consumiendo %d\n", d)
-		} else {
-			fmt.Println("Buffer vacio, esperando!!")
-		}
-		m.Unlock()
-	}
+func consumidor(id int, ch chan int) {
+
 }
 
 func main() {
-	m := new(sync.Mutex)
-	go productor(m)  // 1 proceso
-	go consumidor(m) // 1 proceso
+	ch := make(chan string) // Canal de tipo string
 
-	// pausa
-	var in string
-	fmt.Scanln(&in)
 }
